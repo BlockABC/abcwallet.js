79957d3131b47a4834373d70c0bcdca209cae09c
Link <link@blockabc.com>
2019-09-12 11:06:40 +0800

    feat: add new api to btc, bch chain

--- src/api/BCHAPI.ts
+++ src/api/BCHAPI.ts
@@ -4,6 +4,13 @@ import ChainBaseAPI from './ChainBaseAPI'
 export class BCHAPI extends ChainBaseAPI {
   protected _namespace = 'btc'
 
+  getAddressesFromCard (): Promise<string> {
+    return this._request({
+      method: 'getAddressesFromCard',
+      params: {}
+    })
+  }
+
   sign (params: { rawTransaction: string; unspents: string }): Promise<void> {
     return this._request({
       method: 'sign',
--- src/api/BTCAPI.ts
+++ src/api/BTCAPI.ts
@@ -4,6 +4,13 @@ import ChainBaseAPI from './ChainBaseAPI'
 export class BTCAPI extends ChainBaseAPI {
   protected _namespace = 'btc'
 
+  getAddressesFromCard (): Promise<string> {
+    return this._request({
+      method: 'getAddressesFromCard',
+      params: {}
+    })
+  }
+
   sign (params: { rawTransaction: string; unspents: string }): Promise<void> {
     return this._request({
       method: 'sign',
