207ee586bfba0ef0cc2ab5b2d37bf7d12dedeac7
jeffjing <zgayjjf@qq.com>
2020-02-11 21:05:40 +0800

    feat: add tools to invoke client

--- src/helper.ts
+++ src/helper.ts
@@ -29,3 +29,50 @@ export function verifyParams (params: any, required: string[]): boolean {
 
   return true
 }
+
+/**
+ * 判断当前页面是否被隐藏了
+ */
+export function isDocumentHidden (): boolean {
+  return document.hidden || document.visibilityState === 'hidden'
+}
+
+/**
+ * 唤起 native
+ * @param schema {string|Function} string 的时候是一个要唤起的 schema；Function 时是一个要执行的动作
+ * @param timeout
+ */
+export function invokeNative (schema: string|Function, timeout = 5000): Promise<void> {
+  let hidden = false
+  const startTime = Date.now()
+  const visibilityChangeHandler = () => {
+    if (isDocumentHidden()) {
+      hidden = true
+    }
+  }
+
+  document.addEventListener('visibilitychange', visibilityChangeHandler)
+
+  return new Promise((resolve, reject) => {
+    if (typeof schema === 'string') {
+      window.location.href = schema
+    }
+    if (typeof schema === 'function') {
+      schema()
+    }
+
+    setTimeout(() => {
+      document.removeEventListener('visibilitychange', visibilityChangeHandler)
+
+      const endTime = Date.now()
+      const delayed = endTime - startTime > timeout + 1000 // timeout 的执行是否被延迟了
+
+      if (isDocumentHidden() || hidden || delayed) {
+        resolve()
+      }
+      else {
+        reject(new Error('open failed'))
+      }
+    }, timeout)
+  })
+}
